14fa8e421637d2b6e01cf0148691d9ac5c730577
Keval Kankrecha <keval.kankrecha@kevit.io>
2026-03-07 16:44:19 +0530

    feat: implement dynamic job title display with randomization and adjust font size

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,42 @@ import { Component, Input } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   cartoon = 'assets/header-img.png';
+
+  jobTitles = [
+    'Software Engineer',
+    'Software Developer',
+    'Program Developer',
+    'API Developer',
+    'Web Developer',
+    'FullStack Developer',
+    'Database Engineer',
+    'Node Developer',
+    'Web Engineer',
+    'Data Engineer',
+    'Technical Developer',
+    'AI Integrator',
+  ];
+
+  firstWord = '';
+  secondWord = '';
+
+  ngOnInit() {
+    this.setRandomTitle();
+
+    setInterval(() => {
+      this.setRandomTitle();
+    }, 5000);
+  }
+
+  setRandomTitle() {
+    const random =
+      this.jobTitles[Math.floor(Math.random() * this.jobTitles.length)];
+
+    const parts = random.split(' ');
+
+    this.firstWord = parts[0];
+    this.secondWord = parts.slice(1).join(' ');
+  }
 }
